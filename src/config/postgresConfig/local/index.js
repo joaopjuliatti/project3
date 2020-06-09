890eb7df56366a51735d02988b77875b65f5d49c
Joao Juliatti <joaopjuliatti@gmail.com>
2020-06-09 20:39:18 -0300

    create postgres config and model index

---
+++ src/config/postgresConfig/local/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    username: process.env.LOCAL_POSTGRES_RDS_USERNAME,
+    password: process.env.LOCAL_POSTGRES_RDS_PASSWORD,
+    database: process.env.LOCAL_POSTGRES_RDS_DATABASE,
+    host: process.env.LOCAL_POSTGRES_RDS_HOST,
+    port: process.env.LOCAL_POSTGRES_RDS_PORT,
+    options: {
+      dialect: 'postgres',
+      operatorsAliases: false,
+      logging: false
+    }
+  }
+  
