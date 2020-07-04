5dbc7a869da18e5be7884e44516b5b71f18ea739
Joao Juliatti <joaopjuliatti@gmail.com>
2020-07-04 05:18:01 -0300

    add front-end

---
+++ public/build/precache-manifest.64156f1b81edfb9b4b246f37f7943a91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8d7b16c6402e3c6401b5a63e69bd17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbb77489bb8be05f00b3",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "6a2fb7e8b79c6f5ad8c5",
+    "url": "/static/css/main.7143348d.chunk.css"
+  },
+  {
+    "revision": "cbb77489bb8be05f00b3",
+    "url": "/static/js/2.47073095.chunk.js"
+  },
+  {
+    "revision": "4246f8e66ff822b5dcd4ab8de094e46c",
+    "url": "/static/js/2.47073095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a2fb7e8b79c6f5ad8c5",
+    "url": "/static/js/main.e9f23104.chunk.js"
+  },
+  {
+    "revision": "bf7e486d264229d9d7b0",
+    "url": "/static/js/runtime-main.8b3dbd2d.js"
+  }
+]);
